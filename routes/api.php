614943bd31d9a3f199c79a00b260b540a4166f2d
guga <gugakhachvani@gmail.com>
2024-10-29 21:02:24 +0400

    init

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
+    dd('test');
     return $request->user();
 })->middleware('auth:sanctum');
