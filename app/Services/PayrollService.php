406231e45a76b452e5794a301283eac4255f04c9
guga <gugakhachvani@gmail.com>
2025-02-19 17:49:41 +0400

    sum amount

--- app/Services/PayrollService.php
+++ app/Services/PayrollService.php
@@ -25,43 +25,46 @@ class PayrollService
             $breakdown = json_decode($record->salary_breakdown, true);
             if (!isset($results[$empId])) {
                 $results[$empId] = [
-                    'employee_id' => $empId,
-                    'salary_gross' => 0,
-                    'salary_pension' => 0,
-                    'salary_income_tax' => 0,
-                    'salary_net' => 0,
-    
-                    'sum_benefits_gross' => 0,
+                    'employee_id'          => $empId,
+                    'salary_gross'         => 0,
+                    'salary_pension'       => 0,
+                    'salary_income_tax'    => 0,
+                    'salary_net'           => 0,
+                    'sum_benefits_gross'   => 0,
                     'sum_deductions_gross' => 0,
-    
-                    'position' => $record->position,
-                    'name' => $record->name,
-                    'email' => $record->email,
-                    'phone' => $record->phone,
-                    'id_number' => $record->id_number,
-                    'surname' => $record->surname,
-                    'gender' => $record->gender,
-                    'birth_date' => $record->birth_date,
-                    'bank_account' => $record->bank_account,
-                    'residency' => $record->residency,
-                    'address' => $record->address,
-                    'start_date' => $record->employee_start_date,
-                    'end_date' => $record->employee_end_date,
-                    'pension' => $record->pension,
+                    'position'             => $record->position,
+                    'name'                 => $record->name,
+                    'email'                => $record->email,
+                    'phone'                => $record->phone,
+                    'id_number'            => $record->id_number,
+                    'surname'              => $record->surname,
+                    'gender'               => $record->gender,
+                    'birth_date'           => $record->birth_date,
+                    'bank_account'         => $record->bank_account,
+                    'residency'            => $record->residency,
+                    'address'              => $record->address,
+                    'start_date'           => $record->employee_start_date,
+                    'end_date'             => $record->employee_end_date,
+                    'pension'              => $record->pension,
                 ];
             }
 
-            $results[$empId]['salary_gross'] += (float) $breakdown['base'];
-            $results[$empId]['salary_pension'] += (float) $breakdown['pension'];
+            $results[$empId]['salary_gross']      += (float) $breakdown['base'];
+            $results[$empId]['salary_pension']    += (float) $breakdown['pension'];
             $results[$empId]['salary_income_tax'] += (float) $breakdown['income_tax'];
-            $results[$empId]['salary_net'] += (float) $breakdown['net'];
+            $results[$empId]['salary_net']        += (float) $breakdown['net'];
         }
     
+        // Process Adjustments:
         foreach ($data['adjustments'] as $record) {
             $empId = $record->employee_id;
             $breakdown = json_decode($record->adjustment_breakdown, true);
             $adjustmentName = strtolower(str_replace(' ', '_', $record->adjustment_name)) . '_gross';
-            if (!isset($results[$empId])) continue; 
+            
+            // If the employee hasn't been processed in salaries, skip the adjustment.
+            if (!isset($results[$empId])) {
+                continue;
+            }
     
             if ($record->adjustment_type === 'benefit') {
                 $results[$empId]['sum_benefits_gross'] += (float) $breakdown['base'];
@@ -72,8 +75,16 @@ class PayrollService
             if (!isset($results[$empId][$adjustmentName])) {
                 $results[$empId][$adjustmentName] = 0;
             }
-
+    
             $results[$empId][$adjustmentName] += (float) $breakdown['base'];
+        }
+    
+        // Compute sum_amount = salary_gross + benefits - deductions
+        foreach ($results as $empId => $record) {
+            $results[$empId]['sum_amount'] = 
+                $record['salary_gross'] + 
+                $record['sum_benefits_gross'] - 
+                $record['sum_deductions_gross'];
         }
     
         return array_values($results);
