4a66cde4cab55fe60fa577aed4dc742c01b7d5e6
guga <gugakhachvani@gmail.com>
2025-02-25 11:54:47 +0400

    monthly fixed salaries only

--- app/Repositories/PayrollRepository.php
+++ app/Repositories/PayrollRepository.php
@@ -132,7 +132,7 @@ class PayrollRepository
                     s.daily_salary_calculation_base
                 FROM salaries s
                 WHERE s.employee_id IN ($employeeIDsString)
-                  AND s.type = monthly_fixed
+                  AND s.type = 'monthly_fixed'
                   AND s.start_date <= ? 
                   AND (s.end_date IS NULL OR s.end_date >= ?)
             ),
